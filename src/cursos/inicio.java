a300a3df3bb8065655776a7cf2b4e26e67ff0902
rreyessa <rreyes@swipeapps.io>
2022-01-13 15:14:17 -0600

    commit

--- src/cursos/inicio.java
+++ src/cursos/inicio.java
@@ -3,7 +3,7 @@ public class inicio {
 
 	public static void main(String[] args) {
 		int x = 2;
-		String cad = "hola mundo";
+		String cad = "hola mundo 2";
 		System.out.println(cad);
 		System.out.println(x);
 	}
