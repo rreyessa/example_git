eb5241305b37dedc1f29b217de228f6e0f9f9543
rreyessa <rreyes@swipeapps.io>
2022-01-13 15:22:07 -0600

    prueba

--- src/cursos/inicio.java
+++ src/cursos/inicio.java
@@ -3,7 +3,7 @@ public class inicio {
 
 	public static void main(String[] args) {
 		int x = 2;
-		String cad = "hola mundo 2";
+		String cad = "hola prueba";
 		System.out.println(cad);
 		System.out.println(x);
 	}
