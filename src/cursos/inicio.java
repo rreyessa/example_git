da02b92126561af7cb9816934140487643e29585
rreyessa <rreyes@swipeapps.io>
2022-01-13 14:44:09 -0600

    modificaciones

--- src/cursos/inicio.java
+++ src/cursos/inicio.java
@@ -3,8 +3,8 @@ public class inicio {
 
 	public static void main(String[] args) {
 		int x;
-		String cad;
-		System.out.println("Hola mundo");
+		String cad = "hola mundo";
+		System.out.println(cad);
 	}
 
 }
